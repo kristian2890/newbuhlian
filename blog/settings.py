88292330fc33de14f773b43385465ce32816ad0f
kristian2890 <30598398+kristian2890@users.noreply.github.com>
2025-08-07 13:58:24 +0200

    auto deploy test

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# https 
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
